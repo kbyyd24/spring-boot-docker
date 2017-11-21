b6f05f1a38b28bee639e0fac5777cebbc3a9432c
kbyyd24 <melo@gaoyuexiang.cn>
2017-11-21 21:09:32 +0800

    ignore failure test

--- src/test/java/FailureTest.java
+++ src/test/java/FailureTest.java
@@ -1,8 +1,10 @@
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class FailureTest {
 
     @Test
+    @Ignore
     public void shouldFailed() {
         throw new RuntimeException();
     }
