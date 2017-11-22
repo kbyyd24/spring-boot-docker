a1e27003338e6e20f8cde1850786d6288c42d9d7
kbyyd24 <melo@gaoyuexiang.cn>
2017-11-22 08:14:40 +0800

    add GetMapping to home page controller

--- src/main/java/cn/gaoyuexiang/docker/demo/HomePageController.java
+++ src/main/java/cn/gaoyuexiang/docker/demo/HomePageController.java
@@ -3,14 +3,16 @@ package cn.gaoyuexiang.docker.demo;
 import java.util.Date;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.collect.ImmutableMap;
 
 @RestController
 public class HomePageController {
+
+    @GetMapping
     public Map home() {
-        return ImmutableMap.of("Name", "Spring Boot Demo in Docker", 
-        "Now: ", new Date(System.currentTimeMillis()));
+        return ImmutableMap.of("Name", "Spring Boot Demo in Docker", "Now: ", new Date(System.currentTimeMillis()));
     }
 }
