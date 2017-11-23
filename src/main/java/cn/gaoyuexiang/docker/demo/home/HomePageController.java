2901f1d613aee8aea73b1f815ba3642170b6fa6f
kbyyd24 <melo@gaoyuexiang.cn>
2017-11-23 19:58:53 +0800

    move HomePageController

--- src/main/java/cn/gaoyuexiang/docker/demo/home/HomePageController.java
+++ src/main/java/cn/gaoyuexiang/docker/demo/home/HomePageController.java
@@ -1,4 +1,4 @@
-package cn.gaoyuexiang.docker.demo;
+package cn.gaoyuexiang.docker.demo.home;
 
 import java.util.Date;
 import java.util.Map;
@@ -11,7 +11,7 @@ import com.google.common.collect.ImmutableMap;
 @RestController
 public class HomePageController {
 
-    @GetMapping
+    @GetMapping()
     public Map home() {
         return ImmutableMap.of("Name", "Spring Boot Demo in Docker", "Now: ", new Date(System.currentTimeMillis()));
     }
