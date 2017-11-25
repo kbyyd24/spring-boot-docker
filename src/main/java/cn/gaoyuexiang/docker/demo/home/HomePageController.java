268b3df1085223f1ad3cb692195d3e6bbd9ba53b
kbyyd24 <melo@gaoyuexiang.cn>
2017-11-25 17:38:37 +0800

    use deploy time

--- src/main/java/cn/gaoyuexiang/docker/demo/home/HomePageController.java
+++ src/main/java/cn/gaoyuexiang/docker/demo/home/HomePageController.java
@@ -1,6 +1,7 @@
 package cn.gaoyuexiang.docker.demo.home;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.collect.ImmutableMap;
 
+import static java.time.ZoneId.of;
+
 @RestController
 public class HomePageController {
 
+    private ZonedDateTime deployTime = ZonedDateTime.now(of("Asia/Shanghai"));
+
     @GetMapping()
     public Map home() {
-        return ImmutableMap.of("Name", "Spring Boot Demo in Docker", "Now: ", LocalDateTime.now());
+        return ImmutableMap.of("Name", "Spring Boot Demo in Docker", "Deployed at: ", deployTime);
     }
 }
