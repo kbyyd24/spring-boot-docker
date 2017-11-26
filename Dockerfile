b493b00d0d73c5ca6883ffa08650480282b5b6e2
kbyyd24 <melo@gaoyuexiang.cn>
2017-11-26 23:03:57 +0800

    remove for presentation

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM java
-MAINTAINER Yuexiang Gao
-
-COPY build/libs/spring-docker.jar app.jar
-EXPOSE 8080
-
-ENTRYPOINT ["java", "-jar", "app.jar"]
--- push_image.sh
+++
@@ -1,4 +0,0 @@
-docker build -t yuexiang/spring-boot-demo .
-docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
-docker push yuexiang/spring-boot-demo
-docker logout
