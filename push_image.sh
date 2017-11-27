70070cdc3aa9d2c9f5bed790ddfe26cd6a331c3f
kbyyd24 <melo@gaoyuexiang.cn>
2017-11-27 13:12:54 +0800

    fixed spell mistake

--- push_image.sh
+++ push_image.sh
@@ -1,4 +1,4 @@
-docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
+docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
 docker build -t yuexiang/spring-boot-demo .
 docker push yuexiang/spring-boot-demo
 docker logout
